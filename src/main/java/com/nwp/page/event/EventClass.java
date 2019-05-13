9e2562837aa2c2d4781f4bbd6e29eac53b00badc
rik12s <rakesh.sharma@monotype.com>
2019-05-13 22:26:46 +0530

    Adding Event create code

--- src/main/java/com/nwp/page/event/EventClass.java
+++ src/main/java/com/nwp/page/event/EventClass.java
@@ -1,10 +1,14 @@
 package com.nwp.page.event;
 
 import com.nwp.basePage.NWPBasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.Date;
 
 public class EventClass extends NWPBasePage implements EventConstants {
 
@@ -13,11 +17,20 @@ public class EventClass extends NWPBasePage implements EventConstants {
     @FindBy(xpath=createEventXpath)
     WebElement createEvent;
 
-    @FindBy(xpath = createEventFormXpath)
+    @FindBy(xpath = createEventFormCss)
     WebElement createEventForm;
 
     @FindBy(id=eventTitleNameId)
     WebElement eventTitleName;
+
+    @FindBy(xpath=eventStartDateCss)
+    WebElement eventStartDate;
+
+    @FindBy(css=eventCalenderSetButtonCss)
+    WebElement eventCalenderSetButton;
+
+    @FindBy(xpath=eventEndDateCss)
+    WebElement eventEndDate;
 
     public EventClass(WebDriver driver){
         this.driver = driver;
@@ -26,11 +39,29 @@ public class EventClass extends NWPBasePage implements EventConstants {
 
     public void clickOnCreateEvent(){
         createEvent.click();
-        //wait for createEventform
+        setFluentWait(driver, ExpectedConditions.visibilityOf(createEventForm),10000);
     }
 
-    public void addEventName(){
-        eventTitleName.sendKeys();
+    public void addEventName(String eventName){
+        eventTitleName.sendKeys(eventName);
+    }
+
+    public void setEventStartDate(){
+        eventStartDate.click();
+        driver.switchTo().frame(eventIframeId);
+        eventCalenderSetButton.click();
+        driver.switchTo().defaultContent();
+    }
+
+    public void setEventEndDate(){
+        eventEndDate.click();
+        Date date= new Date();
+        int currentDate = date.getDate();
+        driver.switchTo().frame(eventIframeId);
+        WebElement nextDayOnCalendar = driver.findElement(By.xpath(String.format(eventNextDate,currentDate)));
+        nextDayOnCalendar.click();
+        eventCalenderSetButton.click();
+        driver.switchTo().defaultContent();
     }
 
 }
--- src/main/java/com/nwp/page/event/EventConstants.java
+++ src/main/java/com/nwp/page/event/EventConstants.java
@@ -3,7 +3,17 @@ package com.nwp.page.event;
 public interface EventConstants {
 
     String createEventXpath="//div[@class='lft-panel']//a[@href='/client/add-event']";
-    String createEventFormXpath="//div[@class='eventtickethd border-0']";
+    String createEventFormCss="h2.margin0";
     String eventTitleNameId="mbsc-form-control-2";
+    //String eventStartDateXpath = "//label[text()='Event Starts *']/..//input";
+    //Event Start Date
+    String eventStartDateCss = "input#mobiscroll1557762137123";
+    String eventIframeId = "set_cookie";
+    String eventCalenderSetButtonCss= "div.mbsc-fr-btn1.mbsc-fr-btn-e.mbsc-fr-btn";
+
+    //Event End Date
+    String eventEndDateCss = "input#mobiscroll1557762137124";
+    String eventNextDate = "//div[text()='%1s']";
+    String eventEndDateSetCss= "div.mbsc-fr-btn1.mbsc-fr-btn-e.mbsc-fr-btn";
 
 }
